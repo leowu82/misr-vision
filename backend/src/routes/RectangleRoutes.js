12d45a6f6ff3518c2f4a29bef9a4cc2fb10e47ad
leowu82 <wuzonghua82@gmail.com>
2025-12-08 03:45:01 +0000

    [backend/src/routes/RectangleRoutes.js] add token authentication for post, put, delete

--- backend/src/routes/RectangleRoutes.js
+++ backend/src/routes/RectangleRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const RectController = require('../controllers/RectangleController');
+const authenticateToken = require('../middleware/AuthMiddleware');
 
 router.get('/', RectController.getAllRectangles);
 router.get('/:id', RectController.getAllRectanglesByID);
-router.post('/', RectController.createRectangle);
-router.put('/:id', RectController.updateRectangle);
-router.delete('/:id', RectController.deleteRectangle);
+router.post('/', authenticateToken, RectController.createRectangle);
+router.put('/:id', authenticateToken, RectController.updateRectangle);
+router.delete('/:id', authenticateToken, RectController.deleteRectangle);
 
 module.exports = router;
